83386da2e77aaea2bfe469ad554afef0c9c3fd59
thisisaayush <thakurikarthik@gmail.com>
2023-05-23 21:55:44 -0500

    - completed the components content of shopping app

--- angular-web-app/shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-web-app/shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test.', 'https://www.sainsburysmagazine.co.uk/uploads/media/720x770/07/5537-MilkPudding1120.jpg?v=1-0')
+  ];
+  
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
