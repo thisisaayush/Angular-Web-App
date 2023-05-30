b4775f8b885e1d2cd712c4d37bf7ab45284a78b6
thisisaayush <thakurikarthik@gmail.com>
2023-05-29 23:56:37 -0500

    - added component folders for app development.

---
+++ angular-components/app-component/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerElementComponent,
+    CockpitComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
