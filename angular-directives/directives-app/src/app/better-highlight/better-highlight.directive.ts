840f0306bd709e5ddf62c00bd1278e5cfb807243
thisisaayush <thakurikarthik@gmail.com>
2023-06-18 19:58:39 -0500

    - added responsive and interactive ui features.

---
+++ angular-directives/directives-app/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, OnInit, 
+         Renderer2 , ElementRef,
+         HostListener
+        } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    
+  }
+
+}
